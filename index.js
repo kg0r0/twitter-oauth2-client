e3bd8b7054f8a2fc6856ae835552bb390a6f83f4
kg0r0 <kg0r0@yahoo.co.jp>
2021-10-23 23:56:17 +0900

    refactoring

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const axios = require('axios');
 const config = require('./config.json');
 
 const issuer = new Issuer({
-  'authorization_endpoint': 'https://twitter.com/i/oauth2/authorize',
-  'token_endpoint': 'https://api.twitter.com/2/oauth2/token'
+  authorization_endpoint: 'https://twitter.com/i/oauth2/authorize',
+  token_endpoint: 'https://api.twitter.com/2/oauth2/token'
 });
 
 const client = new issuer.Client({
@@ -29,18 +29,18 @@ app.get('/', (req, res, next) => {
       return res.send('OK!');
     }
     const state = generators.state();
-    const code_verifier = generators.codeVerifier();
-    const code_challenge = generators.codeChallenge(code_verifier);
+    const codeVerifier = generators.codeVerifier();
+    const codeChallenge = generators.codeChallenge(codeVerifier);
     const url = client.authorizationUrl({
       redirect_uri: config.redirect_uri,
       response_type: 'code',
       scope: 'tweet.read users.read offline.access',
       state,
-      code_challenge,
+      code_challenge: codeChallenge,
       code_challenge_method: 'S256',
     })
     req.session.state = state;
-    req.session.code_verifier = code_verifier;
+    req.session.codeVerifier = codeVerifier;
     req.session.originalUrl = req.originalUrl;
     return res.redirect(url);
   })().catch(next);
@@ -52,9 +52,9 @@ app.get('/cb', (req, res, next) => {
       return res.status(403).send('NG');
     }
     const state = req.session.state;
-    const code_verifier = req.session.code_verifier;
+    const codeVerifier = req.session.codeVerifier;
     const params = client.callbackParams(req);
-    const tokenSet = await client.oauthCallback(config.redirect_uri, params, { code_verifier, state });
+    const tokenSet = await client.oauthCallback(config.redirect_uri, params, { code_verifier: codeVerifier, state });
     console.log('received and validated tokens %j', tokenSet);
     req.session.tokenSet = tokenSet;
     return res.redirect(req.session.originalUrl);
@@ -93,6 +93,6 @@ app.get('/revoke', (req, res, next) => {
 });
 
 const port = config.port || 3000;
-app.listen(port, async () => {
+app.listen(port, () => {
   console.log(`Started app on port ${port}`);
 });
